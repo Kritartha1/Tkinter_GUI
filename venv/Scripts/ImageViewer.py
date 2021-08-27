741bf95b445cf46e765eff793b41e9a1ac10aa65
Kritartha1 <kritarthanathofficial@gmail.com>
2021-08-28 01:46:40 +0530

    m

--- venv/Scripts/ImageViewer.py
+++ venv/Scripts/ImageViewer.py
@@ -14,7 +14,8 @@ my_img4=ImageTk.PhotoImage(Image.open("Image/4.jpg"))
 my_img5=ImageTk.PhotoImage(Image.open("Image/5.jpg"))
 
 image_list=[my_img1,my_img2,my_img3,my_img4,my_img5]
-
+status=Label(root,text="Image 1 of "+str(len(image_list)),bd=1,relief=SUNKEN, anchor=E)
+#bd:border
 my_label=Label(image=my_img1)
 my_label.grid(row=0,column=0,columnspan=3)
 
@@ -37,6 +38,9 @@ def forward(image_number):
 
     button_back.grid(row=1, column=0, padx=40, pady=0.1)
     button_forward.grid(row=1, column=2, padx=40, pady=0.1)
+
+    status = Label(root, text="Image "+str(image_number)+" of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
 def back(image_number):
     global my_label
     global button_forward
@@ -56,6 +60,10 @@ def back(image_number):
     button_back.grid(row=1, column=0, padx=40, pady=0.1)
     button_forward.grid(row=1, column=2, padx=40, pady=0.1)
 
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN,
+                   anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
+
 
 button_back=Button(root,text="<<",command=back)
 button_exit=Button(root,text="EXIT PROGRAM",command=root.quit)
@@ -64,6 +72,8 @@ button_forward=Button(root,text=">>", command=lambda:forward(2))
 button_back.grid(row=1,column=0,padx=40,pady=0.1)
 button_exit.grid(row=1,column=1,padx=40,pady=0.1)
 button_forward.grid(row=1,column=2,padx=40,pady=0.1)
+
+status.grid(row=2,column=0,columnspan=3,sticky=W+E)
 
 
 
